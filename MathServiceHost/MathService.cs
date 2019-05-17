556ff844dae3a06895883668d7910fa96d4253a7
AndrasHorinka <horinka.andras@gmail.com>
2019-05-17 12:10:13 +0200

    MathService Main

---
+++ MathServiceHost/MathService.cs
@@ -0,0 +1,28 @@
+﻿using System.ServiceModel;
+using MathTypes;
+
+namespace MathServiceHost
+{
+    public class MathService : IMath
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Substract(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ MathServiceHost/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using MathTypes;
+
+namespace MathServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri LocalAddress = new Uri(ConfigurationManager.AppSettings["baseAddress"]);
+            ServiceHost host = new ServiceHost(typeof(MathService), LocalAddress);
+
+            host.AddServiceEndpoint(typeof(IMath), new BasicHttpBinding(), "math");
+            host.Open();
+
+            Console.WriteLine("MathService is listening to the following");
+            foreach (ServiceEndpoint e in host.Description.Endpoints)
+            {
+                Console.WriteLine("{0} ({1})", e.Address.ToString(), e.Binding.Name);
+            }
+
+            Console.WriteLine("\n Press Enter to terminate...");
+            Console.ReadKey();
+        }
+    }
+}
