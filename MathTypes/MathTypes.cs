f4c6f527248bf4eb21d2e4bd4f6dba2ce2d2ebd6
AndrasHorinka <horinka.andras@gmail.com>
2019-05-17 11:58:20 +0200

    IMath init

---
+++ MathTypes/MathTypes.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+
+namespace MathTypes
+{
+    [ServiceContract]
+    public interface IMath
+    {
+        [OperationContract]
+        double Add(double x, double y);
+        [OperationContract]
+        double Substract(double x, double y);
+        [OperationContract]
+        double Multiply(double x, double y);
+        [OperationContract]
+        double Divide(double x, double y);
+
+    }
+}
